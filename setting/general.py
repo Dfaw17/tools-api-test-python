4257eb5cc208ea4fdccf92db35d394366ba4979b
DaffaPintu <daffa@pintu.co.id>
2023-04-16 15:18:17 +0700

    add setting/general

--- setting/general.py
+++ setting/general.py
@@ -1,21 +1,21 @@
 # POSTKU
 import os
 
-username_postku = ""
-pwd_postku = ""
+username_postku = "admin1"
+pwd_postku = "admin1"
 username_postku_not_found = "admin170901"
 
 # GENERAL
 max_latency = 1000000
 
 # TEST CASE MANAGEMENT
-api_key = ""
+api_key = "8b8d6169357d45082a3c818240449a38b7a56fddafdfe3aa0bdda8a32c5bf632"
 host_test_management = "https://api.qase.io/v1/result"
 test_run = 8
 test_code_project = "TF"
 
 # SLACK NOTIFICATION
-slack_webhook = ""
+slack_webhook = "https://hooks.slack.com/services/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"
 slack_title = os.environ.get('TEST')
 url_artifact = os.environ.get('RUNID')
 notif_slack = "ON"
