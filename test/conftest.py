44521daea4d4bac4e6315150ae499291c0a51a0f
DaffaPintu <daffa@pintu.co.id>
2023-04-17 10:49:10 +0700

    move webhook slack to secret github

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from setting.case_management import *
-from setting.notif_slack import *
 
 
 def pytest_html_report_title(report):
@@ -38,11 +37,11 @@ def hook(request):
 
 @pytest.fixture(scope='session', autouse=True)
 def suite(request):
-    print("\nbefore suite")
+    # print("\nbefore suite")
 
     yield
 
-    print("after suite")
+    # print("after suite")
     test_success = len(request.session.items) - request.session.testsfailed
     test_failed = request.session.testsfailed
     test_all = len(request.session.items)
@@ -53,5 +52,75 @@ def suite(request):
     else:
         color = "2B7A0B"
 
-    webhook_slack(color, test_success, test_failed, test_all, success_rate)
+    # webhook_slack(color, test_success, test_failed, test_all, success_rate)
+    sr = round(success_rate, 2)
 
+    if slack_title is None:
+        title = "Reports Automation Api"
+    else:
+        title = str(slack_title)
+
+    param = {
+        "attachments": [
+            {
+                "color": color,
+                "blocks": [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": title,
+                            "emoji": True
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Success Test:*\n{test_success}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Failed Test:*\n{test_failed}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "*Skipped Test:*\n0"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Total Test:*\n{test_all}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Success Rate:*\n{sr}%"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f"<{url_artifact}|Check Detail Report>"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+
+    header = {
+        "content-type": "application/x-www-form-urlencoded"
+    }
+    requests.post(slack_webhook, json=param, headers=header)
